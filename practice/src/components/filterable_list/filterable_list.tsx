d28c35456c4f827a1363b6a1f297b6699ff97ae6
shiwani dembla <shiwani@alphamudigital.com>
2025-03-01 16:20:27 +0500

    Adds memoization technique

--- practice/src/components/filterable_list/filterable_list.tsx
+++ practice/src/components/filterable_list/filterable_list.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function FilterableList() {
   const [searchTerm, setSearchTerm] = useState("");
   const list = ["apple", "banana", "cherry", "date", "elderberry"];
 
   //each time the input changes, the list is filtered based on the search term
-  const filteredList = list.filter((item) =>
-    item.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+
+  // memo is used to prevent the list from being filtered on every render,
+  // here it's not needed, but it can be useful when the list is large and when we have to apply conditions in the dependency array
+  const filteredList = useMemo(() => {
+    return list.filter((item) =>
+      item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }, [searchTerm]);
+
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
   };
